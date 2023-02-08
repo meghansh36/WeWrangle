32157a270c11ebf4a3110766e731c62fba33f8dc
meghansh36 <meghansh.work@gmail.com>
2023-02-07 22:30:50 -0600

    added cytoscape and basic graph rendering

---
+++ apps/wewrangle-ui/src/editor/components/graphArea/graphArea.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+import * as cy from 'cytoscape'
+
+@Component({
+    selector: 'graph-area',
+    templateUrl: 'graphArea.component.html',
+    styleUrls: ['./graphArea.component.css']
+})
+
+export class GraphAreaComponent implements OnInit {
+    
+    cy:cy.Core = cy({})
+    
+    constructor() { }
+
+    ngOnInit() { 
+
+    this.cy = cy({
+        container: document.getElementById('cytoscape-container'),
+        elements: [ // list of graph elements to start with
+            { // node a
+              data: { id: 'a' }
+            },
+            { // node b
+              data: { id: 'b' }
+            },
+            { // edge ab
+              data: { id: 'ab', source: 'a', target: 'b' }
+            }
+          ],
+        
+        layout: {
+            name: 'grid',
+            rows: 1,
+            padding: 0
+        },
+
+        style: [ // the stylesheet for the graph
+            {
+                selector: 'node',
+                style: {
+                'background-color': '#666',
+                'label': 'data(id)'
+                }
+            },
+        
+            {
+                selector: 'edge',
+                style: {
+                    'width': 3,
+                    'line-color': '#ccc',
+                    'target-arrow-color': '#ccc',
+                    'target-arrow-shape': 'triangle',
+                    'curve-style': 'bezier'
+                }
+            }
+          ],
+
+
+        });
+
+    }
+}
--- apps/wewrangle-ui/src/editor/editor.module.ts
+++ apps/wewrangle-ui/src/editor/editor.module.ts
@@ -5,9 +5,10 @@ import { RouterModule } from '@angular/router'
 import routes from './editor.routing'
 import { ToolPaneComponent } from './components/toolPane/toolPane.component'
 import { ConfigSidebarComponent } from './components/configSidebar/configSidebar.component'
+import { GraphAreaComponent } from './components/graphArea/graphArea.component'
 
 @NgModule({
-  declarations: [EditorComponent, ToolPaneComponent, ConfigSidebarComponent],
+  declarations: [EditorComponent, ToolPaneComponent, ConfigSidebarComponent, GraphAreaComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes)
